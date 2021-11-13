55c9e8ab9fa989845ca4f697ce10b7aea2e95c93
Axel Engblom <axelengblom94@gmail.com>
2021-11-13 14:31:13 +0100

    added JSON data

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 const data = require("../balls.json");
 
-let manufacturers = {"1": ["storm", "ebonite", "brunswick"],}
+let manufacturers = { 1: ["storm", "ebonite", "brunswick"] };
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.header("Content-Type",'application/json');
+router.get("/", function (req, res) {
+  res.header("Content-Type", "application/json");
   res.send(data);
+});
+
+router.get("/:manufacturer", function (req, res) {
+  res.header("Content-Type", "application/json");
+  let manufacturer = req.params.manufacturer;
+  res.send(data[manufacturer]);
 });
 
 module.exports = router;
