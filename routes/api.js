fe5339fc3d0d0fac9dd0c5359031ea95ca3e48d0
Axel Engblom <axelengblom94@gmail.com>
2021-11-13 20:42:55 +0100

    remaking the json because of errors, and started work on getting balls by attributes

--- routes/api.js
+++ routes/api.js
@@ -11,10 +11,25 @@ router.get("/", function (req, res) {
   res.send(data);
 });
 
-router.get("/:manufacturer", function (req, res) {
+router.get("/balls/:manufacturer", function (req, res) {
   res.header("Content-Type", "application/json");
-  let manufacturer = req.params.manufacturer;
-  res.send(data[manufacturer]);
+  for (i = 0; i < data.length; i++) {
+    if (data[i].Manufacturer == req.params.manufacturer) {
+      console.log(true);
+      res.send(data[i].Balls);
+    }
+  }
+});
+
+router.get("/balls/covers/:coverstock", function (req, res) {
+  res.header("Content-Type", "application/json");
+  let coverstock = req.params.coverstock;
+
+  const foundBalls = {};
+
+  for (let i = 0; i < Object.keys(data).length; i++) {}
+
+  res.send(console.log("Fish"));
 });
 
 module.exports = router;
