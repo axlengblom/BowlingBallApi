5b1ef5dd1c40403bc45df81ecde68f9a5a071024
axel engblom <axel.engblom@medieinstitutet.se>
2021-11-15 20:55:56 +0100

    added more manufacturers to data.json and added some comments for the code

--- routes/api.js
+++ routes/api.js
@@ -10,9 +10,11 @@ router.get("/", function (req, res) {
   res.send(data);
 });
 
+//Gets all the balls from a specific manufacturer
 router.get("/balls/:manufacturer", function (req, res) {
   res.header("Content-Type", "application/json");
 
+  //loops through the manufacturers to find the one that matches the criteria
   for (i = 0; i < data.length; i++) {
     if (data[i].Manufacturer.toUpperCase() == req.params.manufacturer.toUpperCase()) {
       res.send(data[i].Balls);
@@ -20,12 +22,17 @@ router.get("/balls/:manufacturer", function (req, res) {
   }
 });
 
+//Gets all the balls from all the manufacturers with a specific coverstock 
 router.get("/balls/covers/:coverstock", function (req, res) {
   res.header("Content-Type", "application/json");
 
   let arrayOfBalls = [];
 
+
   let coverstock = req.params.coverstock.toUpperCase();
+
+  //loops through the balls to find the ones that match the critera and adds them to an array that is then displayed
+  
   for (i = 0; i< data.length; i++) {
     for (j = 0; j < data[i].Balls.length; j++) {
       if (data[i].Balls[j].Specs.Coverstock.toUpperCase() == coverstock){
